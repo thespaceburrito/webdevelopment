4ef5177a27dc2ec5e6f7f82b11e78bebe98f5070
thespaceburrito <97800462+thespaceburrito@users.noreply.github.com>
2022-02-02 00:57:09 -0600

    Add files via upload

---
+++ jolocafe/Javascript/script.js
@@ -0,0 +1,12 @@
+var b;
+b = prompt("Please enter your name");
+document.getElementById("Message").innerHTML = "Hello" + " " + b;
+
+var a= "Jonathan";
+var b= "Obi";
+var initials= a.substr(0,1) + "." + b.substr(0,1) + "." ;
+document.getElementById("initials").innerHTML = initials
+
+
+var numberEntered = document.getElementById("enterNumber.value")
+
